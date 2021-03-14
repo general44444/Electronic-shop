5685c40694b80daaeb2db157c9ec309d6c11168a
general44444 <aabd55389@gmail.com>
2021-03-14 10:29:05 -0700

    Edit

--- Electronic shop/Forms/Home.Designer.cs
+++ Electronic shop/Forms/Home.Designer.cs
@@ -45,9 +45,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(23, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.Size = new System.Drawing.Size(190, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "label11111111111";
             // 
             // panel1
             // 
